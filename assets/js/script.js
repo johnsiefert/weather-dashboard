458016f43207555777f6480f7711401830d513f2
johnsiefert <johnsief12@gmail.com>
2021-11-14 19:49:24 -0500

    made funcition for API key

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+//Declare a variable to store the searched city
+let city="";
+// variable declaration
+let searchCity = $("#search-city");
+let searchButton = $("#search-button");
+let clearButton = $("#clear-history");
+let currentCity = $("#current-city");
+let currentTemperature = $("#temperature");
+let currentHumidty= $("#humidity");
+let currentWSpeed=$("#wind-speed");
+let currentUvindex= $("#uv-index");
+let sCity=[];
+// searches the city to see if it exists in the entries from the storage
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+//Set up the API key
+let APIKey="af86bdf5ff77ca275e24b969af70f4c1";
+// Display the curent and future weather to the user after grabing the city form the input text box.
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
